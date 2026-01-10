c16cdd242bd7e840fc15ea21985c93eb4c987916
João Pedro Teófilo da Silva <168291293+Joaopts10@users.noreply.github.com>
2026-01-09 21:37:23 -0300

    Refactor memory access and instruction handling

--- main.c
+++ main.c
@@ -24,8 +24,8 @@ typedef struct {
 } CPU;
 
 int16_t extensao_sinal(uint16_t val, int bits) {
-    uint16_t mask = 1 << (bits - 1);
-    return (val ^ mask) - mask;
+    uint16_t mascara = 1 << (bits - 1);
+    return (val ^ mascara) - mascara;
 }
 
 void verificar_memoria(uint16_t end) {
@@ -44,14 +44,12 @@ void imprimir_estado_cpu(CPU *cpu) {
     printf("C = %d\n", cpu->flag_c);
 
     for (int i = 0; i < TAMANHO_MEMORIA; i++) {
-        bool eh_pilha = (cpu->regs[SP] != 0x2000) &&
-                        (i >= cpu->regs[SP]) &&
+        bool eh_pilha = (cpu->regs[SP] != 0x2000) && 
+                        (i >= cpu->regs[SP]) && 
                         (i <= 0x1FFF);
 
         if (cpu->mem_acessada[i] && !eh_pilha) {
-            printf("[ 0x%04hX ] = 0x%04hX\n",
-                   (unsigned short)i,
-                   (unsigned short)cpu->memoria[i]);
+            printf("[ 0x%04hX ] = 0x%04hX\n", (unsigned short)i, (unsigned short)cpu->memoria[i]);
         }
     }
 
@@ -59,15 +57,11 @@ void imprimir_estado_cpu(CPU *cpu) {
         int sp = cpu->regs[SP];
         if (sp < TAMANHO_MEMORIA) {
             for (int i = 0x1FFF; i >= sp; i--) {
-                printf("[ 0x%04hX ] = 0x%04hX\n",
-                       (unsigned short)i,
-                       (unsigned short)cpu->memoria[i]);
+                printf("[ 0x%04hX ] = 0x%04hX\n", (unsigned short)i, (unsigned short)cpu->memoria[i]);
             }
         }
     }
 }
-
-
 
 void cpu_inicializar(CPU *cpu) {
     memset(cpu->regs, 0, sizeof(cpu->regs));
@@ -80,8 +74,6 @@ void cpu_inicializar(CPU *cpu) {
     cpu->regs[PC] = 0;
 }
 
-
-
 void carregar_programa(CPU *cpu, const char *arquivo) {
     FILE *f = fopen(arquivo, "r");
     if (!f) {
@@ -91,13 +83,21 @@ void carregar_programa(CPU *cpu, const char *arquivo) {
 
     uint16_t end, val;
     while (fscanf(f, "%hx %hx", &end, &val) == 2) {
-        if (end < TAMANHO_MEMORIA)
+        if (end < TAMANHO_MEMORIA) {
             cpu->memoria[end] = val;
+        }
     }
     fclose(f);
 }
 
 void cpu_executar(CPU *cpu, int num_bps, int *breakpoints) {
+    uint16_t cond, pc_atual, end, dado;
+    int16_t imm10;
+    uint32_t res;
+    bool saltar;
+    int ch;
+    short tmp;
+
     while (1) {
         for (int i = 0; i < num_bps; i++) {
             if (cpu->regs[PC] == breakpoints[i]) {
@@ -111,63 +111,66 @@ void cpu_executar(CPU *cpu, int num_bps, int *breakpoints) {
         }
 
         cpu->ir = cpu->memoria[cpu->regs[PC]];
-        uint16_t pc_atual = cpu->regs[PC];
+        pc_atual = cpu->regs[PC];
         cpu->regs[PC]++;
 
         uint16_t opcode = cpu->ir & 0xF;
-        uint16_t rd     = (cpu->ir >> 12) & 0xF;
-        uint16_t rm     = (cpu->ir >> 8) & 0xF;
-        uint16_t rn     = (cpu->ir >> 4) & 0xF;
+        uint16_t rd = (cpu->ir >> 12) & 0xF;
+        uint16_t rm = (cpu->ir >> 8) & 0xF;
+        uint16_t rn = (cpu->ir >> 4) & 0xF;
 
         uint16_t imm4_baixo = (cpu->ir >> 4) & 0xF;
-        uint16_t imm4_alto  = (cpu->ir >> 12) & 0xF;
-        int16_t  imm8       = extensao_sinal((cpu->ir >> 4) & 0xFF, 8);
-        int16_t  imm12      = extensao_sinal((cpu->ir >> 4) & 0x0FFF, 12);
+        uint16_t imm4_alto = (cpu->ir >> 12) & 0xF;
+        int16_t imm8 = extensao_sinal((cpu->ir >> 4) & 0xFF, 8);
+        int16_t imm12 = extensao_sinal((cpu->ir >> 4) & 0x0FFF, 12);
 
         switch (opcode) {
             case 0x0:
                 cpu->regs[PC] = cpu->regs[PC] + imm12;
                 break;
+
             case 0x1:
-            {
-                uint16_t cond = (cpu->ir >> 14) & 0x3;
-                int16_t imm10 = extensao_sinal((cpu->ir >> 4) & 0x03FF, 10);
-                bool saltar = false;
+                cond = (cpu->ir >> 14) & 0x3;
+                imm10 = extensao_sinal((cpu->ir >> 4) & 0x03FF, 10);
+                saltar = false;
 
                 switch (cond) {
-                    case 0: saltar = (cpu->flag_z == 1); break;
-                    case 1: saltar = (cpu->flag_z == 0); break;
-                    case 2: saltar = (cpu->flag_z == 0 && cpu->flag_c == 1); break;
-                    case 3: saltar = (cpu->flag_z == 1 || cpu->flag_c == 0); break;
+                    case 0: 
+                        saltar = (cpu->flag_z == 1); 
+                        break;
+                    case 1: 
+                        saltar = (cpu->flag_z == 0); 
+                        break;
+                    case 2: 
+                        saltar = (cpu->flag_z == 0 && cpu->flag_c == 1); 
+                        break;
+                    case 3: 
+                        saltar = (cpu->flag_z == 1 || cpu->flag_c == 0); 
+                        break;
                 }
 
                 if (saltar) {
                     cpu->regs[PC] = cpu->regs[PC] + imm10;
                 }
                 break;
-            }
 
             case 0x4:
                 cpu->regs[rd] = imm8;
                 break;
 
             case 0x5:
-            {
-                uint32_t res = (uint32_t)cpu->regs[rm] + (uint32_t)cpu->regs[rn];
+                res = (uint32_t)cpu->regs[rm] + (uint32_t)cpu->regs[rn];
                 cpu->regs[rd] = (uint16_t)res;
                 cpu->flag_z = (cpu->regs[rd] == 0);
                 cpu->flag_c = (res > 0xFFFF);
                 break;
-            }
 
             case 0x6:
-            {
-                uint32_t res = (uint32_t)cpu->regs[rm] + imm4_baixo;
+                res = (uint32_t)cpu->regs[rm] + imm4_baixo;
                 cpu->regs[rd] = (uint16_t)res;
                 cpu->flag_z = (cpu->regs[rd] == 0);
                 cpu->flag_c = (res > 0xFFFF);
                 break;
-            }
 
             case 0x7:
                 cpu->flag_c = (cpu->regs[rm] < cpu->regs[rn]);
@@ -207,50 +210,39 @@ void cpu_executar(CPU *cpu, int num_bps, int *breakpoints) {
                 break;
 
             case 0x2:
-            {
-                uint16_t end = cpu->regs[rm] + imm4_baixo;
-
+                end = cpu->regs[rm] + imm4_baixo;
                 if (end >= 0xF000) {
                     if (end == IO_CHAR_ENTRADA) {
-                        int ch = getchar();
+                        ch = getchar();
                         cpu->regs[rd] = (uint16_t)(unsigned char)ch;
                         printf("IN %c\n", (char)cpu->regs[rd]);
-                    }
-                    else if (end == IO_INT_ENTRADA) {
-                        short tmp;
+                    } else if (end == IO_INT_ENTRADA) {
                         scanf("%hd", &tmp);
                         cpu->regs[rd] = (uint16_t)tmp;
                         printf("IN %hd\n", tmp);
                     }
-                }
-                else {
+                } else {
                     verificar_memoria(end);
                     cpu->mem_acessada[end] = true;
                     cpu->regs[rd] = cpu->memoria[end];
                 }
                 break;
-            }
 
             case 0x3:
-            {
-                uint16_t end = cpu->regs[rm] + imm4_alto;
-                uint16_t dado = cpu->regs[rn];
-
+                end = cpu->regs[rm] + imm4_alto;
+                dado = cpu->regs[rn];
                 if (end >= 0xF000) {
                     if (end == IO_CHAR_SAIDA) {
                         printf("OUT %c\n", (char)dado);
-                    }
-                    else if (end == IO_INT_SAIDA) {
+                    } else if (end == IO_INT_SAIDA) {
                         printf("OUT %hd\n", (short)dado);
                     }
-                }
-                else {
+                } else {
                     verificar_memoria(end);
                     cpu->mem_acessada[end] = true;
                     cpu->memoria[end] = dado;
                 }
                 break;
-            }
 
             case 0xE:
                 cpu->regs[SP]--;
@@ -270,8 +262,7 @@ void cpu_executar(CPU *cpu, int num_bps, int *breakpoints) {
                 break;
 
             default:
-                printf("Instrucao desconhecida: 0x%04hX em PC=0x%04hX\n",
-                       (unsigned short)cpu->ir, (unsigned short)pc_atual);
+                printf("Instrucao desconhecida: 0x%04hX em PC=0x%04hX\n", (unsigned short)cpu->ir, (unsigned short)pc_atual);
                 imprimir_estado_cpu(cpu);
                 return;
         }
@@ -304,6 +295,8 @@ int main(int argc, char *argv[]) {
 
     cpu_executar(&cpu, num_bps, breakpoints);
 
-    if (breakpoints) free(breakpoints);
+    if (breakpoints) {
+        free(breakpoints);
+    }
     return 0;
 }
